8db7a6e9182e4038112184efa13e75cfeb9f159e
Nick Pillitteri <nick@tshlabs.org>
2020-06-17 20:20:35 -0400

    Copyright update

--- killer.go
+++ killer.go
@@ -1,6 +1,6 @@
 // Killer - Repeatedly try to kill a process
 //
-// Copyright 2017 TSH Labs
+// Copyright 2017 Nick Pillitteri
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
